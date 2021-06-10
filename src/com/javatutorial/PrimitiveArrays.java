6cfdf8903adea04653133c78f948d225466e88dd
Joshua Limbing <joshuacharleslimbing@gmail.com>
2021-06-11 00:39:28 +0800

    additional files

--- src/com/javatutorial/PrimitiveArrays.java
+++ src/com/javatutorial/PrimitiveArrays.java
@@ -1,4 +1,18 @@
 package com.javatutorial;
 
 public class PrimitiveArrays {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+
+        numbers[0] = 5;
+        numbers[1] = 2;
+        numbers[2] = 3;
+
+        for (int i = 0; i < 3; i++){
+            System.out.println(numbers[i]);
+        }
+
+    }
+
 }
---
+++ src/com/javatutorial/PrimitiveArraysExercise.java
@@ -0,0 +1,4 @@
+package com.javatutorial;
+
+public class PrimitiveArraysExercise {
+}
